b522e1616e378848c69bc0b022a66c44025d29c5
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-11 01:37:42 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-  speed.to_i > 60 || speed.to_i < 40 ? true : false
+  speed.to_i > 60 || speed.to_i < 40 ? true : false  
 end
 
 
 
 def not_safe?(speed)
-
+	speed > 60 || speed < 40 ? true : false
 end
+	
+
+
